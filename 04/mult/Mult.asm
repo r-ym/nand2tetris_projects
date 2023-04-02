6cc4670dc0dca5ab1324007b039541646f33e1f1
r-ym <ramanyachi@gmail.com>
2023-04-02 17:44:23 +0900

    chap4

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,36 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+@R0
+D=M
+
+@R3
+M=D
+
+@R2
+M=0
+
+(MULTI)
+
+@R3
+D=M
+
+@END
+D;JEQ
+
+@R1
+D=M
+
+@R2
+M=M+D
+
+@R3
+M=M-1
+
+@MULTI
+0;JMP
+
+(END)
+@END
+0;JMP
